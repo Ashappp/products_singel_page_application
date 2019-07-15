11e55ee82d684c0b51bd3a32692cdcee8460da95
ashappp <you@example.com>
2019-07-15 11:55:30 +0300

    fixed bug discription

--- client/src/components/ProductCard/ProductCard.jsx
+++ client/src/components/ProductCard/ProductCard.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import s from './ProductCard.module.css';
 
-const ProductCard = ({ name, price, description, _id }) => {
+const ProductCard = ({ name, price, discription, _id }) => {
   return (
     <article className={s.article} key={_id}>
       <h2>Name:{name}</h2>
       <span>Price: {price}</span>
-      <p>Discription:{description}</p>
+      <p>Discription:{discription}</p>
       <Link className={s.link} to={`/products`}>
         Back to Products
       </Link>
